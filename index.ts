7ab82da3aa505d167df5916f36d736dd96ecc7af
metatron020NaNa03 <malikmanan97@gmail.com>
2021-01-23 23:26:49 +0530

    Ts-function-type

--- index.ts
+++ index.ts
@@ -27,6 +27,17 @@ console.log(buzz);
 
 // object types
 
+// Type Assignment (DONT's)
+
+let objUnion: {
+  name: string;
+  age: number;
+} = {
+  name: "Manan",
+  age: 56
+};
+
+//   Type Inference (Do'S)
 const person = {
   name: "Manan",
   age: 23
@@ -58,3 +69,59 @@ console.log(Role.AUTHOR); // OUTPUT=0 ..(0th Index)
 Role.ADMIN === testInput.Role
   ? console.log("Dashboard Open")
   : console.log("Get Lost");
+
+// Union Type
+let unionExample: (string | number)[] = ["hello", 1, "two", 256];
+
+console.log(unionExample.reverse());
+
+// type-alising
+
+type myTypeAliasing = string | number | Boolean;
+
+let typeAliaisingExample: myTypeAliasing[] = ["hello", 1, "two", 256, true];
+
+// Functions and Voids
+
+function logger(num1: number): void {
+  console.log(num1);
+  if (2 == 2) {
+    return console.log("jello");
+  }
+}
+console.log(logger(sum(15, 25)));
+
+// Function types
+
+function sumOne(num1: number, num2: number) {
+  return num1 * num2;
+}
+let myFunctionType: (a: number, b: number) => number;
+myFunctionType = sumOne;
+// myFunctionType = sum;
+console.log(myFunctionType(8, 8));
+
+// function call-backs
+
+let myCallbackFunctionSlate: (
+  a: number,
+  b: number,
+  callBack: (num: number) => void
+) => void;
+
+function myCallbackFunction(
+  num1: number,
+  num2: number,
+  cb: (num3: number) => void
+) {
+  let result = num1 + num2;
+  cb(result);
+}
+
+myCallbackFunctionSlate = myCallbackFunction;
+
+console.log(
+  myCallbackFunctionSlate(1, 18, result => {
+    console.log(result);
+  })
+);
